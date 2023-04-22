0297cd80d95c8606a6d457f9da0602660048d617
Nikita112431fsf <131515930+Nikita112431fsf@users.noreply.github.com>
2023-04-22 12:21:37 +0500

    Тема Урока:Dictionary

---
+++ main.py
@@ -0,0 +1,12 @@
+books = { "room" : "комната",
+          "mercedes" : "Gls",
+          "key" : "ключ",
+           "door" : "дверь",
+          }
+print(books)
+books["lamborghini"]= "Huracan"
+books["lada"]= "granta"
+print(books)
+del books["key"]
+del books["door"]
+print(books)
